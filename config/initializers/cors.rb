1fb7747da2a340df04298a2db0079bcad758b56f
Alex Nunes <alexnunessilva006@gmail.com>
2025-05-31 20:27:26 -0300

    fix: rack cors accept front end request

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:5173'  # origem do seu frontend
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true  # se estiver usando cookies, sessões, etc.
+  end
+end
