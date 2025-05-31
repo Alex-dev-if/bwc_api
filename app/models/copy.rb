adeda9a8b0815334200022aa1c2b2179e565f48a
Alex Nunes <alexnunessilva006@gmail.com>
2025-05-31 14:05:17 -0300

    feat: copies crud

---
+++ app/models/copy.rb
@@ -0,0 +1,16 @@
+class Copy < ApplicationRecord
+  after_create :set_copy_code
+
+  def set_copy_code
+    year = created_at.strftime("%Y")
+    month = created_at.strftime("%m")
+    day = created_at.strftime("%d")
+    copies_count = ActiveRecord::Base.connection.execute("SELECT nextval('copy_code_int')")
+                                    .first['nextval']
+                                    .to_s
+                                    .rjust(4, '0')
+
+    self.code = "#{year}#{month}#{day}#{copies_count}"
+    save
+  end
+end
