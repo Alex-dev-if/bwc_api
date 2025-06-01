174010252ee25d1747b938ce1cbde90fe3dc29e8
Alex Nunes <alexnunessilva006@gmail.com>
2025-06-01 12:15:24 -0300

    feat: users index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :landings
   resources :copies
   resources :books
-  resources :users, only: [:create]
+  resources :users, only: [:create, :index]
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 end
