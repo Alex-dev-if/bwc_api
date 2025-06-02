78844fe05e3b41cb074a8352a81e43a06ead0d5a
Alex Nunes <alexnunessilva006@gmail.com>
2025-06-02 16:17:53 -0300

    feat: edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :set_user, only: %i[ update destroy ]
 
   def index
     @users = User.all
     render json: @users
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: { message: 'User updated successfully', user: @user }, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def create
@@ -17,8 +26,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @users = User.find(params[:id])
-    @users.destroy!
+    @user.destroy!
     
     respond_to do |format|
       format.json { head :no_content }
@@ -26,6 +34,10 @@ class UsersController < ApplicationController
   end
   
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :role)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :landings
   resources :copies
   resources :books
-  resources :users, only: [:create, :index, :destroy]
+  resources :users, only: [:create, :index, :destroy, :update]
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get '/users/:user_id/fines', to: 'fines#by_user'
